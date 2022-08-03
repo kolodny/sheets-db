5508ce3094b46a0dbc120ef42ce14bb8ae2bdd38
Moshe Kolodny <mkolodny@netflix.com>
2022-08-03 17:58:14 -0400

    close on redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './app';
+import './firebase/app';
+import { getAuth, getRedirectResult } from 'firebase/auth';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+getRedirectResult(getAuth()).then(async (userCredential) => {
+  if (userCredential) {
+    // Do some DB stuff
+    window.close();
+    return;
+  }
+  const React = await import('react');
+  const ReactDOM = await import('react-dom/client');
+  const { App } = await import('./app');
+
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
