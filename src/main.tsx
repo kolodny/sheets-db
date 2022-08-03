4331b75808543f6b899aa09765c6bf5267a9167d
Moshe Kolodny <mkolodny@netflix.com>
2022-08-03 18:33:14 -0400

    same opening

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import './firebase/app';
 import { getAuth, getRedirectResult } from 'firebase/auth';
 
+const open = () => {
+  const width = 500;
+  const height = 600;
+
+  const left = screen.availWidth / 2 - width / 2;
+  const top = screen.availHeight / 2 - height / 2;
+
+  const opened = window.open(
+    'http://localhost:5173/',
+    '_blank',
+    `left=${left},top=${top},width=${width},height=${height}`
+  );
+};
+
+(window as any).open2 = open;
+
 getRedirectResult(getAuth()).then(async (userCredential) => {
   if (userCredential) {
     // Do some DB stuff
-    window.close();
+    close();
     return;
   }
   const React = await import('react');
