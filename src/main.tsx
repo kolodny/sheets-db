ac7810d6e11f287fbd2f8b4679e0cdbeeec8fca4
Moshe Kolodny <mkolodny@netflix.com>
2022-08-05 11:36:47 -0400

    no top level await

--- src/main.tsx
+++ src/main.tsx
@@ -21,24 +21,27 @@ const open = () => {
 const params = new URLSearchParams(window.location.search);
 const origin = params.get('origin');
 if (origin) {
-  const userCredential = await getRedirectResult(getAuth());
-  if (userCredential) {
-    const token = params.get('token');
-    window.opener.postMessage({ type: 'login', token }, origin);
-  } else {
-    const token = crypto.randomUUID();
-    // Google will redirect back with the URL token param.
-    history.pushState(null, '', `${location.search}&token=${token}`);
-    signInRedirect();
-  }
+  getRedirectResult(getAuth()).then(async (userCredential) => {
+    if (userCredential) {
+      const token = params.get('token');
+      window.opener.postMessage({ type: 'login', token }, origin);
+    } else {
+      const token = crypto.randomUUID();
+      // Google will redirect back with the URL token param.
+      history.pushState(null, '', `${location.search}&token=${token}`);
+      signInRedirect();
+    }
+  });
 } else {
-  const React = await import('react');
-  const ReactDOM = await import('react-dom/client');
-  const { App } = await import('./app');
-
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  const imports = Promise.all([
+    import('react'),
+    import('react-dom/client'),
+    import('./app'),
+  ]).then(([React, ReactDOM, { App }]) => {
+    ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  });
 }
