e7c87e7e3439adc196b79c0a8855b06b5ce2d107
Moshe Kolodny <mkolodny@netflix.com>
2022-08-05 13:13:43 -0400

    lib build

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: 'src/sdk.ts',
+      name: 'sdk',
+      fileName: 'sdk',
+    },
+  },
 });
